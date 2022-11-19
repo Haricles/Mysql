c6220e5a1d840244b0d214f3ee7786b2adf9acb4
Harján Krisztián <erik00000@hotmail.com>
2022-11-19 10:19:27 +0100

    elso gyakorlo feladat

---
+++ Gyakorlat_01/main.sql
@@ -0,0 +1,9 @@
+Írj SQL utasítást amely az online tesztadatbázis 'Categories' táblájából lekérdezi
+a, az összes adatot,
+b, a 'CategoryName' oszlopba tartozó adatokat,
+c, a 'CategoryName' és 'Description' oszlopba tartozó adatokat!
+
+
+a: SELECT * FROM Categories;
+b: SELECT CategoryName FROM Categories;
+c: SELECT CategoryName,Description FROM Categories;
