24bb8effdcb4db50f30c9b7b4f1dabeed832b231
Harján Krisztián <erik00000@hotmail.com>
2023-06-22 22:44:08 +0200

    gyak_16

--- Gyakorlat_16/main.sql
+++ Gyakorlat_16/main.sql
@@ -1,10 +1,5 @@
-A CREATE, DROP, ALTER című lecke 1. feladatában létrehozott kutymenhely adatbazist egeszitsd 
-ki egy a kutya gazdájának adatait tároló táblával! A két táblát kapcsold össze, úgy hogy a 
-most létrehozott tábla legyen a gyermektábla! 
-A táblákat töltsd fel néhány adattal!
+Jelenítsd meg a 'B' betűvel kezdődő előadók nevét és dalait!
 
-
-CREATE TABLE gazdi_adatok (gazdi_id INT PRIMARY KEY AUTO_INCREMENT,id INT,nev VARCHAR(30),
-kor INT,FOREIGN KEY (id) REFERENCES kutya_adatok2(id));
-
-INSERT INTO gazdi_adatok (nev,kor) VALUES("Julia",22);
+SELECT eloadok.nev,dalok.cim
+FROM eloadok,dalok
+WHERE eloadok.eloadoid = dalok.eloadoid AND eloadok.nev LIKE "A%";
